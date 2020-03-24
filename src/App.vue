79b7ed0e4be407c0c631581d886a64abcc7c7e15
Hugo Carrasco <hugo@bunsan.io>
2020-03-23 20:45:19 -0600

    Create a Single File Component

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <CardItem
+      name="Java"
+      :show-details="true"
+      description="Es un lenguaje de programación orientado a objetos que se incorporó al ámbito de la informática en los años noventa. La idea de Java es que pueda realizarse programas con la posibilidad de ejecutarse en cualquier contexto, en cualquier ambiente, siendo así su portabilidad uno de sus principales logros."
+      url-image="https://live.mrf.io/statics/i/ps/www.muylinux.com/wp-content/uploads/2020/03/java.png" />
+  </div>
+</template>
+
+<script>
+import CardItem from "@/components/CardItem";
+export default {
+  name: "App",
+  components:{
+    CardItem
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  display: flex;
+  margin-top: 64px;
+  justify-content: center;
+}
+</style>
---
+++ src/components/CardItem.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="card">
+      <img 
+        :src="urlImage"
+        :alt="name" 
+        style="width:100%">
+
+      <div class="container">
+        <div class="card_name"><b>{{ name }}</b></div>         
+      </div>
+
+      <div class="card_description">
+        {{ description }}
+      </div>
+
+      <div class="card_details" v-if="showDetails">
+        <a :class="[{'card_details_link' : showDetails}]"  href="https://www.oracle.com/mx/java/">
+          READ MORE
+        </a>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import {isValidURL} from '@/utils/validations'
+
+export default {
+  name: "CardItem",
+  props:{
+    name:{
+      type: String,
+      required: true
+    },
+    description:{
+      type: String,
+      default: ''
+    },    
+    urlImage:{
+      type: String,
+      required: true,
+      validator: function(value){
+        return isValidURL(value)
+      }
+    },
+    showDetails:{
+      type: Boolean,
+      default: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  width: 40%;
+}
+
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+
+.container {
+  padding: 2px 16px;
+}
+
+.card_name{
+  font-size: 26px;
+  color: #4f4f4f;
+  text-align: center;
+  padding-top: 20px;
+}
+
+.card_description{
+  font-size: 16px;
+  color: #4f4f4f;
+  padding-top: 20px;
+  padding-bottom: 15px;
+}
+
+.card_details_link{
+  color: red;
+  text-shadow: 1px 1px 2px black;
+}
+
+.card_details{
+  padding-bottom: 20px;
+  text-align: center;
+}
+
+.card_description {
+  padding: 0px 30px 30px;
+  text-align: justify;
+}
+
+</style>
